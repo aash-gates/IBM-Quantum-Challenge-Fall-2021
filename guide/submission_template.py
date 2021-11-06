4505c1a5e75124ddc87f6b054b79121da49a662a
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-11-06 07:15:44 +0530

    Update submission_template.py

--- guide/submission_template.py
+++ guide/submission_template.py
@@ -3,3 +3,4 @@ Challenge 4c solver function for the IBM Quantum Fall Challenge 2021
 Author: Firstname Lastname <xxxxx@email.com> # TODO 1: change to your name and email
 Score: 312345 # TODO 1: change to your score
 
+TODO:
