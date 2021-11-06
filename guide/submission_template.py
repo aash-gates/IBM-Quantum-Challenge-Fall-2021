a2577aba7be62f87434b2247d5356d8a43bb30ec
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-11-06 07:16:31 +0530

    Update submission_template.py

--- guide/submission_template.py
+++ guide/submission_template.py
@@ -4,3 +4,4 @@ Author: Firstname Lastname <xxxxx@email.com> # TODO 1: change to your name and e
 Score: 312345 # TODO 1: change to your score
 
 TODO:
+1. Add author name, email and score at the top of the file
